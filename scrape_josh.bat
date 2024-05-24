dcd62019f17941d98fd156cd4a3070456de3e525
Jason <jason@50skid.com>
2024-05-24 01:18:37 -0700

    Bug fixes

--- scrape_josh.bat
+++ scrape_josh.bat
@@ -1,1 +1,7 @@
+@echo off
+
+if exist .venv\Scripts\activate.bat (
+    call .venv\Scripts\activate.bat
+)
+
 python scrape_linkedin.py --url="https://www.linkedin.com/in/joshuabrandau/recent-activity/all/" --excel --openai
